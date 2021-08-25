663878d7114d7358820800c387facc5e6c1144b1
K-Ohgata <oh.kazuma1017@gmail.com>
2021-08-25 13:15:49 +0900

    save1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  greet(isMorning){
+    if(isMorning){
+      return<span>Good Morning</span>
+    }else{
+      return<span>Hello</span>
+    }
+  }
+
+  render(){
+    const hoge='hogehoge'
+    const list=[
+      {id:1,title:'one'},
+      {id:2,title:'two'},
+      {id:3,title:'three'},
+      {id:4,title:'four'},
+      {id:5,title:'five'},
+    ]
+    const num=list.map((val,index)=>{return<div key={val.id}>IDは{val.id},TITLEは{val.title}</div>})
+    const guusuu=list.filter((val,index)=>{return val.id%2===0})
+    const num2=guusuu.map((val,index)=>{return <div key={val.id}>IDは{val.id},TITLEは{val.title}</div>})
+    const isMorning=true;
+
+    function Greeting(){
+      if(isMorning){
+        return<span>Good Morning</span>
+      }else{
+        return<span>Hello</span>
+      }
+    }
+    
+    return(
+      <div>
+        {/* <h1>Hello</h1> */}
+        <p className="hoge">{hoge}</p>
+        <div>{num}</div>
+        <div>{num2}</div>
+        <div>
+        {(()=>{
+          if(isMorning){
+            return<span>Good Morning</span>
+          }else{
+            return<span>Hello</span>
+          }
+        })()}
+        </div>
+        <div>{isMorning&&<span>Good Morning</span>}</div>
+        <div>{isMorning?<span>Good Morning</span>:<span>Hello</span>}</div>
+        <Greeting/>
+        <div>{this.greet(isMorning)}</div>
+        <button onClick={()=>console.log('Woooooaaaaaa!!!')}>Greet</button>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
